8a8fde2d537c36a095052f2066cc8cc00c3234a8
Wasin P <pwnlowed@icloud.com>
2016-06-07 09:22:18 +0000

    Simple refactor

--- getdaze.js
+++ getdaze.js
@@ -3,43 +3,58 @@ var cheerio = require('cheerio');
 var request = require('request');
 var moment = require('moment');
 
-//Functions
-//Callback (error, result)
-module.exports.getJson = function(baseUrl, elementId, elements, callback) {
-    var url = baseUrl + elementId;
-    var output = moment();
+//Get data class + constructor
+function Data(baseUrl, elementId) {
+  this.baseUrl = baseUrl;
+  this.elementId = elementId;
+};
 
-    request(url, function(error, response, html) {
-        if (error)
-            return callback(true, null);
+//Function in data class
+Data.prototype.getData = function(elementItem,callback) {
+  var url = this.baseUrl + this.elementId;
+  var output = moment();
 
-        //Download HTML
-        var $ = cheerio.load(html);
+  request(url, function(error, response, html) {
+    if (error)
+      return callback(true, error);
 
-        //Ready for dynamic key adding
-        var json = {};
+    //Download HTML
+    var $ = cheerio.load(html);
 
-        //Date is mandatory key
-        json.date = output.toString();
+    //Ready for dynamic key adding
+    var json = {};
 
-        //Loop through elements
-        //Element [{type: 'class or id',classid: 'element name', key: 'name of the field'}]
-        for (var element of elements) {
-            var classid = "";
-            if (element.type == 'id') {
-                classid = '#' + element.classid;
-            } else {
-                classid = '.' + element.classid;
-            }
-            if ($(classid).length) {
-                $(classid).filter(function() {
-                    var data = $(this);
-                    //json[element.key] = data.text().replace(/(\r\n|\n|\r)/gm, "").trim();
-                    json[element.key] = data.text().trim();
-                });
-            }
-        }
+    //Date is mandatory key
+    json.date = output.toString();
 
-        callback(false, json);
-    });
+    //Loop through elements
+    //Element [{type: 'class or id',classid: 'element name', key: 'name of the field'}]
+    for (var element of elementItem) {
+      var classid = "";
+      if (element.type == 'id') {
+        classid = '#' + element.classid;
+      } else {
+        classid = '.' + element.classid;
+      }
+      if ($(classid).length) {
+        $(classid).filter(function() {
+          var data = $(this);
+          //json[element.key] = data.text().replace(/(\r\n|\n|\r)/gm, "").trim();
+          json[element.key] = data.text().trim();
+        });
+      }
+    }
+
+    callback(false, json);
+  });
+};
+
+//callback(err, result)
+module.exports.getJson = function(baseUrl, elementId, elementItems, callback) {
+  var data = new Data(baseUrl, elementId);
+  data.getData(elementItems, function(err,result){
+    if (err)
+      return callback(true,err);
+    callback(false,result);
+  });
 };
